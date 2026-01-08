78343d4e7c248ab3b2da62e2a756bb93eb6fada8
hhrris665-tech <hhrris665@gmail.com>
2026-01-08 05:15:02 +0300

    Add ITF Colored Belt Syllabus and Dan Patterns

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
 import React, { useState } from "react";
 
-const patterns = [
-  { name: "Chon-Ji", moves: 19, meaning: { en: "Heaven and Earth", om: "jannata fi lafa" } },
-  { name: "Dan-Gun", moves: 21, meaning: { en: "Founder of Korea", om: "Hundeeffataa Kooriyaa" } },
-  { name: "Do-San", moves: 24, meaning: { en: "Patriot", om: "Lammii bilisummaa" } },
-  { name: "Won-Hyo", moves: 28, meaning: { en: "Buddhist monk", om: "Barsiisaa Buudaa" } },
-  { name: "Yul-Gok", moves: 38, meaning: { en: "Great scholar", om: "Beekaa guddaa" } },
-  { name: "Joong-Gun", moves: 32, meaning: { en: "Patriot Ahn Joong-Gun", om: "Gootoo bilisummaa" } },
-  { name: "Toi-Gye", moves: 37, meaning: { en: "Scholar Yi Hwang", om: "Beekaa seenaa" } },
-  { name: "Hwa-Rang", moves: 29, meaning: { en: "Youth warriors", om: "Dargaggoota loltoota" } },
-  { name: "Choong-Moo", moves: 30, meaning: { en: "Admiral Yi Sun-Sin", om: "Ajajaa lolaa" } },
-
-  { name: "Kwang-Gae", moves: 39, meaning: { en: "Great king", om: "Mootii guddaa" } },
-  { name: "Po-Eun", moves: 36, meaning: { en: "Faithful scholar", om: "Beekaa amanamaa" } },
-  { name: "Ge-Baek", moves: 44, meaning: { en: "General Ge-Baek", om: "Ajajaa waraanaa" } },
-
-  { name: "Eui-Am", moves: 45, meaning: { en: "Independence leader", om: "Hogganaa bilisummaa" } },
-  { name: "Choong-Jang", moves: 52, meaning: { en: "General Kim Duk Ryang", om: "Ajajaa gooticha" } },
-  { name: "Juche", moves: 45, meaning: { en: "Self-reliance", om: "Of-dandeessummaa" } },
-  { name: "Sam-Il", moves: 33, meaning: { en: "March 1st movement", om: "Sochii bilisummaa" } },
-  { name: "Yoo-Sin", moves: 68, meaning: { en: "General Kim Yoo-Sin", om: "Ajajaa seenaa" } },
-  { name: "Choi-Yong", moves: 46, meaning: { en: "General Choi Yong", om: "Ajajaa waraanaa" } },
-  { name: "Yon-Gae", moves: 49, meaning: { en: "General Yon Gae", om: "Ajajaa gooticha" } },
-  { name: "Ul-Ji", moves: 42, meaning: { en: "General Ul-Ji", om: "Ajajaa loltoota" } },
-  { name: "Moon-Moo", moves: 61, meaning: { en: "King Moon-Moo", om: "Mootii walitti-buusi" } },
-  { name: "So-San", moves: 72, meaning: { en: "Patriot monk", om: "Barsiisaa goota" } },
-  { name: "Se-Jong", moves: 24, meaning: { en: "King Se-Jong", om: "Mootii qubee" } }
+const belts = [
+  {
+    geup: "10th Geup",
+    color: "White Belt",
+    pattern: "None",
+    om: "10ffaa – Qal’ee adii – Hin jiru"
+  },
+  {
+    geup: "9th Geup",
+    color: "White–Yellow Belt",
+    pattern: "Chon-Ji",
+    om: "9ffaa – Adii-balleessa – Chon-Ji"
+  },
+  {
+    geup: "8th Geup",
+    color: "Yellow Belt",
+    pattern: "Dan-Gun",
+    om: "8ffaa – Balleessa – Dan-Gun"
+  },
+  {
+    geup: "7th Geup",
+    color: "Yellow–Green Belt",
+    pattern: "Do-San",
+    om: "7ffaa – Balleessa-Magariisa – Do-San"
+  },
+  {
+    geup: "6th Geup",
+    color: "Green Belt",
+    pattern: "Won-Hyo",
+    om: "6ffaa – Magariisa – Won-Hyo"
+  },
+  {
+    geup: "5th Geup",
+    color: "Green–Blue Belt",
+    pattern: "Yul-Gok",
+    om: "5ffaa – Magariisa-Cuquliisa – Yul-Gok"
+  },
+  {
+    geup: "4th Geup",
+    color: "Blue Belt",
+    pattern: "Joong-Gun",
+    om: "4ffaa – Cuquliisa – Joong-Gun"
+  },
+  {
+    geup: "3rd Geup",
+    color: "Blue–Red Belt",
+    pattern: "Toi-Gye",
+    om: "3ffaa – Cuquliisa-Diimaa – Toi-Gye"
+  },
+  {
+    geup: "2nd Geup",
+    color: "Red Belt",
+    pattern: "Hwa-Rang",
+    om: "2ffaa – Diimaa – Hwa-Rang"
+  },
+  {
+    geup: "1st Geup",
+    color: "Red–Black Belt",
+    pattern: "Choong-Moo",
+    om: "1ffaa – Diimaa-Gurraacha – Choong-Moo"
+  }
 ];
 
 export default function App() {
@@ -40,19 +75,40 @@ export default function App() {
         </button>
       </div>
 
-      <h3>ITF Patterns (Tuls)</h3>
+      <h3>🎗️ ITF Colored Belt Syllabus (Geup)</h3>
+
       <ul>
-        {patterns.map((p, i) => (
-          <li key={i}>
-            <b>{p.name}</b> – {p.meaning[lang]} ({p.moves} moves)
+        {belts.map((b, i) => (
+          <li key={i} style={{ marginBottom: 8 }}>
+            <b>{b.geup}</b> – {lang === "en"
+              ? `${b.color} | Pattern: ${b.pattern}`
+              : b.om}
           </li>
         ))}
       </ul>
 
       <hr />
+
       <p><b>Created by:</b> Ahmed Muhammad</p>
       <p><b>Instructor:</b> Sayimak Ibrahim</p>
       <p><b>Assistance:</b> ChatGPT (OpenAI)</p>
     </div>
   );
 }
+<h3>⚫ ITF Black Belt Patterns (Dan)</h3>
+<ul>
+  {danPatterns.map((d, i) => (
+    <li key={i}>
+      <b>{d.dan}</b> – {d.pattern}
+    </li>
+  ))}
+</ul>
+const danPatterns = [
+  { dan: "1st Dan", pattern: "Chon-Ji (Review)" },
+  { dan: "2nd Dan", pattern: "Kwang-Gae, Po-Eun, Ge-Baek" },
+  { dan: "3rd Dan", pattern: "Eui-Am" },
+  { dan: "4th Dan", pattern: "Choong-Jang, Juche, Sam-Il" },
+  { dan: "5th Dan", pattern: "Yoo-Sin" },
+  { dan: "6th Dan", pattern: "Choi-Yong, Yon-Gae, Ul-Ji, Moon-Moo" },
+  { dan: "7th Dan", pattern: "So-San, Se-Jong" }
+];
